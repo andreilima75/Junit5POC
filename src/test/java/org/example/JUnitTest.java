cb1de38ba19dc99a284c2073756c7b12a158ac1a
Andrei Lima <andrei.lima75@gmail.com>
2026-06-04 15:53:07 -0300

    junit5 poc

--- src/test/java/org/example/JUnitTest.java
+++ src/test/java/org/example/JUnitTest.java
@@ -3,6 +3,8 @@ package org.example;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
@@ -70,6 +72,19 @@ public class JUnitTest {
     @Test
     @EnabledIfEnvironmentVariable(named = "CI", matches = "true")
     void onlyOnCI() {
+        assertTrue(true);
+    }
+
+    @Test
+    @DisabledIfSystemProperty(named = "java.version", matches = "1\\..*")
+    void onlyOnNewJava() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Tag("slow")
+    @Tag("integration")
+    void taggedTest() {
         assertTrue(true);
     }
 }
